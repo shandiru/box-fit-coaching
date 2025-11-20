564731dabc7d6304f48102fa203b58c3791ee9cf
DirushanDev <rubakumardirushan@gmail.com>
2025-11-20 09:10:28 +0530

    added

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     return (
         <div>
             <Hero />
-             {/* <About /> */}
+             <About />
              <TrainingClasses />
              <ScheduleSection />
               {/* <WhyChooseUs /> */}
